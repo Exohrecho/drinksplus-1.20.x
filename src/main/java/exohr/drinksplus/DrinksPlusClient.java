75f40ab3e699521fbfe1e35007b18251daf703ce
Exohrecho <robertflorescu2006@gmail.com>
2024-01-03 15:23:42 +0200

    Initial commit

---
+++ src/main/java/exohr/drinksplus/DrinksPlusClient.java
@@ -0,0 +1,10 @@
+package exohr.drinksplus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DrinksPlusClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

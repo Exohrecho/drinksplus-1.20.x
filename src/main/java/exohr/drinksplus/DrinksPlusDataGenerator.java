b471524018a58144f6d1a389241cc3e0bf78779c
Exohrecho <robertflorescu2006@gmail.com>
2024-01-03 14:39:56 +0200

    Initial commit

---
+++ src/main/java/exohr/drinksplus/DrinksPlusDataGenerator.java
@@ -0,0 +1,11 @@
+package exohr.drinksplus;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DrinksPlusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
